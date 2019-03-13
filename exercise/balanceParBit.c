432fa90fcc54fb0b53c950dfa516e859e85ed597
Siyin Zhou <siyinz0208@gmail.com>
2019-03-13 12:15:56 +1100

    Add sort

--- exercise/balanceParBit.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-
-//using a bit string
-//enum to store {([
--- sorting/testSort.c
+++ sorting/testSort.c
@@ -4,22 +4,13 @@
 typedef int Item;
 
 void bubbleSort(Item a[], int nitems);
+void testBubble(void);
+void selectionSort(Item a[], int nitems); 
+void testSelection(void);
 
 int main(int argc, char* argv[]) {
-    int test[] = {3, 5, 2, 8, 9, 10, 15, 1};
-    printf("Initial:");
-    for(int i = 0; i < 8; i++) {
-        printf("%d ", test[i]);
-    }
-    printf("\n\n");
-
-    bubbleSort(test, 8);
-    printf("After bubble:");
-    for(int i = 0; i < 8; i++) {
-        printf("%d ", test[i]);
-    }
-    printf("\n\n");
-
+    testBubble();
+    testSelection();
     return 0;
 }
 
@@ -35,4 +26,56 @@ void bubbleSort(Item a[], int nitems) {
         }
         x++;
     }
+}
+
+void testBubble() {
+    int test[] = {3, 5, 2, 8, 9, 10, 15, 1};
+    printf("Initial:\n");
+    for(int i = 0; i < 8; i++) {
+        printf("%d ", test[i]);
+    }
+    printf("\n\n");
+
+    bubbleSort(test, 8);
+    printf("After Bubble Sort :\n");
+    for(int i = 0; i < 8; i++) {
+        printf("%d ", test[i]);
+    }
+    printf("\n\n");
+
+}
+
+
+void selectionSort(Item a[], int nitems) {
+    Item *min = NULL;
+    int x = 0;
+    while(x < nitems) {
+        min = &a[x];
+        for(int i = x + 1; i < nitems; i++) {
+            if(*min > a[i]) {
+                 min = &a[i];
+    	      }
+        }
+        Item b = a[x];
+        a[x] = *min;
+	  *min = b;
+        x++;
+    }
+}
+
+void testSelection(void) {
+    int test[] = {3, 5, 2, 8, 9, 10, 15, 1};
+    printf("Initial:\n");
+    for(int i = 0; i < 8; i++) {
+        printf("%d ", test[i]);
+    }
+    printf("\n\n");
+
+    selectionSort(test, 8);
+    printf("After Selection Sort :\n");
+    for(int i = 0; i < 8; i++) {
+        printf("%d ", test[i]);
+    }
+    printf("\n\n");
+
 }
